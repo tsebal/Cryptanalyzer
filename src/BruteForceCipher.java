8c812d428e62695fe19668f7e200b8f5625a0a50
Tsebal <atsebal@gmail.com>
2022-04-22 17:12:55 +0400

    Added new class BruteForceCipher, method bruteForce, methods description, menu item, loop menu for console, new static final error message.

---
+++ src/BruteForceCipher.java
@@ -0,0 +1,78 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * BruteForceCipher
+ * Selects the encryption key for the Caesar encryption algorithm
+ * by brute force from the provided ALPHABET and the encrypted text.
+ *
+ */
+
+public class BruteForceCipher {
+    private Character[] alphabet;
+    private char[] decodedText;
+    private String[] plainText;
+    private List<Character> alphabetList;
+    private Path inputFilePath;
+
+    public BruteForceCipher(Path inputFilePath, String alphabet) {
+        //converts Alphabet String to Character array in the upper case format
+        this.alphabet = alphabet.toUpperCase().chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        this.alphabetList = Arrays.asList(this.alphabet);
+        this.plainText = new String[alphabet.length()];
+        this.inputFilePath = inputFilePath;
+    }
+
+    //Produces plain text with encryption key by direct matching
+    private String[] producePlaintext(String cipherText) {
+        //put each letter of the ciphertext in an array of characters in the upper case format
+        char[] message = cipherText.toUpperCase().toCharArray();
+        //loop through all the keys
+        for (int key = 0; key < alphabet.length; key++) {
+            //set the value of the decrypted array of characters to be the same as the length of the cipher text
+            decodedText = new char[message.length];
+            //loop through the characters of the ciphertext
+            for (int i = 0; i < message.length; i++) {
+
+                //if character is in the alphabetList
+                if (alphabetList.contains(message[i])) {
+                    //shift the letters
+                    decodedText[i] = alphabet[(alphabetList.indexOf(message[i]) + key) % alphabet.length];
+                } else {
+                    decodedText[i] = message[i];
+                }
+            }
+            //Decrypted text with one of the keys (key = Array cell number)
+            plainText[key] = String.valueOf(decodedText);
+        }
+        return plainText;
+    }
+
+    //Finds a cipher key by direct matching and returns it
+    public int findKey() throws IOException {
+        //reads encrypted text from the file to string
+        byte[] encryptedFileContent = Files.readAllBytes(Paths.get(inputFilePath.toString()));
+        String encryptedText = new String(encryptedFileContent, StandardCharsets.UTF_8);
+        //run the search and put all calculated variants into in String array
+        String[] decryptedText = producePlaintext(encryptedText);
+        //The encryption key you are looking for
+        int key = 0;
+        //calculate the key from the founded variants with natural text criteria
+        for (int i = 0; i < decryptedText.length; i++) {
+            key = alphabet.length - i;
+            if (decryptedText[i].contains(". ") && decryptedText[i].contains(", ")) {
+                System.out.println("Ключ подобран! Ключ = " + key + "\n");
+                break;
+            } else {
+                System.out.print("Ключ = " + key + " не подходит. " +
+                                    "Возможно закодированная информация не является логичным текстом.\r");
+            }
+        }
+        return key;
+    }
+}
--- src/Cryptanalizer.java
+++ src/Cryptanalizer.java
@@ -5,32 +5,43 @@ import java.util.Scanner;
 
 /**
  * Cryptanalizer
- * Converts text from the ALPHABET by Caesar's cipher.
+ * Encrypts and decrypts the provided text with Caesar's algorithm from the provided ALPHABET.
+ * Selects an encryption key for the encrypted text based on the provided ALPHABET.
  *
  * @author Tsebal
- * @since 0.1b
+ * @since 0.11b
  */
 
 public class Cryptanalizer {
     private static final String INCORRECT_PATH_MSG = "Некорректный путь к файлу, введите корректный путь:";
+    private static final String ERROR_MSG = "Возникла непредвиденная ошибка. Перезапустите программу.";
     public static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\":-!? ";
 
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в криптоанализатор!\nРабота производится с шифром Цезаря.\n" +
-                "Пожалуйста сделайте свой выбор (введите цифру соответствующую пункту меню):\n" +
-                "1) Шифровать текст\n" +
-                "2) Дешифровать текст\n" +
-                "3) Выход");
+        //console menu
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        Scanner scanner = new Scanner(System.in);
-        int menuChoice = scanner.nextInt();
-        switch (menuChoice) {
-            case 1 -> encrypt();
-            case 2 -> decrypt();
-            case 3 -> System.exit(0);
-            default -> System.out.println("Вы сделали некорректный выбор.");
+            while (true) {
+                System.out.println("Добро пожаловать в криптоанализатор!\nРабота производится с шифром Цезаря.\n" +
+                        "Пожалуйста сделайте свой выбор (введите цифру соответствующую пункту меню):\n" +
+                        "1) Шифровать текст\n" +
+                        "2) Дешифровать текст\n" +
+                        "3) Подбор ключа шифрования к зашифрованному тексту (brute force)\n" +
+                        "4) Выход");
+
+                int menuChoice = scanner.nextInt();
+                switch (menuChoice) {
+                    case 1 -> encrypt();
+                    case 2 -> decrypt();
+                    case 3 -> bruteForce();
+                    case 4 -> System.exit(0);
+                    default -> System.out.println("Вы сделали некорректный выбор.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(ERROR_MSG);
         }
-        scanner.close();
     }
 
     //method encrypts entered file to entered output file with key
@@ -50,7 +61,7 @@ public class Cryptanalizer {
 
     //method decrypts entered file to output file with key
     private static void decrypt() {
-        System.out.println("Введите полный путь к файлу для расшифровки: ");
+        System.out.println("Введите полный путь к зашифрованному файлу: ");
         Path inputFilePath = pathResolver("input");
 
         System.out.println("Введите полный путь к файлу для вывода результатов расшифровки: ");
@@ -61,6 +72,16 @@ public class Cryptanalizer {
         System.out.println("Расшифровываем содержимое исходного файла в \n" + outputFilePath.toString());
         String result = encryptorDecryptor(inputFilePath, outputFilePath, k, 1);
         System.out.println(result);
+    }
+
+    //Finds the encryption key by direct selection of the key to the encrypted text
+    private static void bruteForce() throws IOException {
+        System.out.println("Введите полный путь к зашифрованному файлу для подбора ключа шифрования: ");
+        Path inputFilePath = pathResolver("input");
+
+        System.out.println("Подбираем ключ, ждите...");
+        BruteForceCipher bruteForceCipher = new BruteForceCipher(inputFilePath, ALPHABET);
+        bruteForceCipher.findKey();
     }
 
     //gets secret algorithm key from console
@@ -99,7 +120,7 @@ public class Cryptanalizer {
                 }
             }
         } catch (Exception e) {
-            System.out.println("Возникла непредвиденная ошибка, перезапустите программу.");
+            System.out.println(ERROR_MSG);
             e.printStackTrace();
             System.exit(-1);
         }
@@ -115,12 +136,12 @@ public class Cryptanalizer {
     public static String encryptorDecryptor(Path inputFile, Path outputFile, int key, int method) {
         //creates reader/writer for encrypt/decrypt chars with algorithm
         try (Reader reader = new BufferedReader(
-                new InputStreamReader(
-                        new FileInputStream(inputFile.toString()), "UTF-8"));
+                            new InputStreamReader(
+                            new FileInputStream(inputFile.toString()), "UTF-8"));
              Writer writer = new BufferedWriter(
-                     new OutputStreamWriter(
-                             new FileOutputStream(outputFile.toString()), "UTF-8"))) {
-            int charOffset = 0;                             //char at new position with entered key
+                            new OutputStreamWriter(
+                            new FileOutputStream(outputFile.toString()), "UTF-8"))) {
+            int charOffset = 0;                             //char at new position in ALPABET with entered key
 
             //cycle reads one char per round
             while (reader.ready()) {
@@ -160,6 +181,6 @@ public class Cryptanalizer {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "Операция прошла успешно.";
+        return "Операция выполнена успешно.";
     }
 }
